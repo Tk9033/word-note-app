1612bad8b5bd4065a874880e2ff81b0321e1c1e9
Tk9033 <koday021369@gmail.com>
2025-08-23 22:40:58 +0900

    Revert "ログイン、ログアウト機能実装完了"

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,5 @@
 class Deck < ApplicationRecord
-  belongs_to :user
   has_many :cards, dependent: :destroy
   has_many :study_sessions, dependent: :destroy
-  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 50 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
 end
--- test/controllers/study_sessions_controller_test.rb
+++ test/controllers/study_sessions_controller_test.rb
@@ -2,21 +2,11 @@ require "test_helper"
 
 class StudySessionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
-
-    # fixtures に decks(:one) があれば使う。無ければ新規作成。
-    deck_record = (decks(:one) rescue nil)
-    @deck = deck_record.is_a?(Deck) ? deck_record : Deck.create!(name: "DeckOne")
-
-    # セッション作成がスキップされないよう最低2枚カードを用意
-    @deck.cards.create!(question: "Q1", answer: "A1") if @deck.cards.empty?
-    @deck.cards.create!(question: "Q2", answer: "A2") if @deck.cards.size < 2
-
-    log_in_as(@user)
+    @deck = decks(:one)
   end
 
   test "should create session" do
-    assert_difference "StudySession.count", +1 do
+    assert_difference("StudySession.count", 1) do
       post deck_study_sessions_path(@deck)
     end
     assert_response :redirect
@@ -25,46 +15,36 @@ class StudySessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show session" do
-    post deck_study_sessions_path(@deck)
-    study_session = @deck.study_sessions.order(:created_at).last
-
-    get deck_study_session_path(@deck, study_session)
+    session = @deck.study_sessions.create!(status: :in_progress, current_index: 0)
+    get deck_study_session_path(@deck, session)
     assert_response :success
   end
 
   test "should answer and advance" do
-    post deck_study_sessions_path(@deck)
-    study_session = @deck.study_sessions.order(:created_at).last
+    session = @deck.study_sessions.create!(status: :in_progress, current_index: 0)
+    current_card = session.current_card
+    assert current_card, "current_card が nil です。deck(:one) にカードがあるか確認してください。"
 
-    current_card = study_session.current_card || @deck.cards.first
-    post answer_deck_study_session_path(@deck, study_session),
-         params:  { card_id: current_card.id, correct: true }
+    post answer_deck_study_session_path(@deck, session),
+         params: { card_id: current_card.id, correct: true }
+
     assert_response :redirect
 
-    study_session.reload
-    assert_equal 1, study_session.current_index
+    session.reload
+    assert_equal 1, session.current_index
   end
 
   test "should go back" do
-    post deck_study_sessions_path(@deck)
-    study_session = @deck.study_sessions.order(:created_at).last
-
-    current = study_session.current_card || @deck.cards.first
-    post answer_deck_study_session_path(@deck, study_session),
-         params:  { card_id: current.id, correct: true }
-    study_session.reload
-    assert_equal 1, study_session.current_index
-
-    post back_deck_study_session_path(@deck, study_session)
-    study_session.reload
-    assert_equal 0, study_session.current_index
+    session = @deck.study_sessions.create!(status: :in_progress, current_index: 1)
+    post back_deck_study_session_path(@deck, session)
+    assert_response :redirect
+    session.reload
+    assert_equal 0, session.current_index
   end
 
   test "should show result" do
-    post deck_study_sessions_path(@deck)
-    study_session = @deck.study_sessions.order(:created_at).last
-
-    get result_deck_study_session_path(@deck, study_session)
+    session = @deck.study_sessions.create!(status: :in_progress, current_index: 0)
+    get result_deck_study_session_path(@deck, session)
     assert_response :success
   end
 end
