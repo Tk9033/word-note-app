c14d123f7bbaf08930bb4aeee58f9052a41a79fd
Tk9033 <koday021369@gmail.com>
2025-08-29 20:39:33 +0900

    CIエラー修正

--- db/migrate/20250828224008_add_user_ref_to_decks.rb
+++ db/migrate/20250828224008_add_user_ref_to_decks.rb
@@ -1,5 +1,15 @@
 class AddUserRefToDecks < ActiveRecord::Migration[7.2]
   def change
-    add_reference :decks, :user, null: false, foreign_key: true
+    unless column_exists?(:decks, :user_id)
+      add_reference :decks, :user, null: false, foreign_key: true
+    else
+      unless foreign_key_exists?(:decks, :users)
+        add_foreign_key :decks, :users
+      end
+      unless index_exists?(:decks, :user_id)
+        add_index :decks, :user_id
+      end
+      change_column_null :decks, :user_id, false
+    end
   end
 end
